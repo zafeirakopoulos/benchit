92fd457fb520eec31a6d9391e495e810daed3c89
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2013-11-22 20:13:33 +0100

    ignore

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: iso-8859-1 -*-
 
 from benchit import *
 import time
@@ -8,27 +8,27 @@ import sys
 
 import glob
 import json
-import pandas
-pandas.set_option('display.notebook_repr_html', True)
-from IPython.display import HTML
+#import pandas
+#pandas.set_option('display.notebook_repr_html', True)
+#from IPython.display import HTML
 
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 
 
-##benchfolder=os.path.join("..","partition-analysis-data",sys.argv[1])
-#benchfolder=os.path.join(sys.argv[1])
-#b=Bench(benchfolder)
-#b.read_methods()
-#b.read_instances()
-#b.read_datasets()
-#b.create_benchmarks()
+#benchfolder=os.path.join("..","partition-analysis-data",sys.argv[1])
+benchfolder=os.path.join(sys.argv[1])
+b=Bench(benchfolder)
+b.read_methods()
+b.read_instances()
+b.read_datasets()
+b.create_benchmarks()
 
-#b.run_benchmarks(600)
-#b. generate_json_output()
-#of=OutputFactory(b)
-#of.generate_rest()
+b.run_benchmarks(600)
+b. generate_json_output()
+of=OutputFactory(b)
+of.generate_rest()
 
-#of.generate_html()
+of.generate_html()
 
-##b.remove_tmp()
-#b.create_tarball()
+#b.remove_tmp()
+b.create_tarball()
